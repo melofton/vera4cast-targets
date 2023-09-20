427a5df3f6bd0c0bb26db3f17c39d89ecd842744
Quinn Thomas <rqthomas@vt.edu>
2023-09-20 08:35:12 -0400

    updating bucket

--- targets/exo_targets.R
+++ targets/exo_targets.R
@@ -8,9 +8,8 @@ fcr_files <- c("https://pasta.lternet.edu/package/data/eml/edi/271/7/71e6b946b75
 bvr_files <- c("https://raw.githubusercontent.com/FLARE-forecast/BVRE-data/bvre-platform-data-qaqc/bvre-waterquality_L1.csv",
                "https://pasta.lternet.edu/package/data/eml/edi/725/3/a9a7ff6fe8dc20f7a8f89447d4dc2038")
 
+s3 <- arrow::s3_bucket("bio230121-bucket01", endpoint_override = "renc.osn.xsede.org")
+s3_stage2$CreateDir("vera4cast/targets/daily")
 
-exo_daily <- target_generation_exo_daily(fcr_files, bvr_files)
-
-
-s3 <- arrow::s3_bucket("vera4cast-targets/aquatics_daily", endpoint_override = "sdsc.osn.xsede.org")
+s3 <- arrow::s3_bucket("bio230121-bucket01/vera4cast/targets/daily", endpoint_override = "renc.osn.xsede.org")
 arrow::write_csv_arrow(exo_daily, sink = s3$path("exo_daily-targets.csv.gz"))
